f639679b17f150dc5bbd420c69552cccb9a0520d
hsyntes <se.hsyntes@gmail.com>
2023-08-20 13:02:45 +0300

    creating playlists

---
+++ src/components/playlist/CreatePlaylistModal.js
@@ -0,0 +1,56 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Input from "../ui/Input";
+import Modal from "../ui/Modal";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import useInput from "../../hooks/useInput";
+import searchMusics from "../../utils/searchMusics";
+import { useQuery } from "react-query";
+import getRecommendedMusics from "../../utils/getRecommendedMusics";
+
+const CreatePlaylistModal = ({ modal, handleModal }) => {
+  const {
+    state: { value: search, isValid: isSearchValid },
+    handleOnChange: handleSearchOnChange,
+  } = useInput();
+
+  const { data: searchedMusics, isLoading: isSearchMusicsLoading } = useQuery(
+    ["searchMusics", search],
+    {
+      queryFn: () => {
+        if (isSearchValid) return searchMusics(search);
+      },
+    }
+  );
+
+  const { data: recommendedMusics, isLoading: isRecommendedMusicsLoading } =
+    useQuery({
+      queryKey: "getRecommendedMusics",
+      queryFn: getRecommendedMusics,
+    });
+
+  return (
+    <Modal show={modal} className="w-5/6 lg:w-2/4 py-8">
+      <Modal.Header>
+        <Input
+          type="text"
+          name="search"
+          value={search}
+          onChange={handleSearchOnChange}
+          placeholder="Search musics"
+          className="!bg-white dark:!bg-dark"
+        />
+        <FontAwesomeIcon
+          icon={faTimes}
+          size="xl"
+          className="cursor-pointer ms-auto"
+          onClick={handleModal}
+        />
+      </Modal.Header>
+      <Modal.Body>
+        <h6 className="font-semibold">Suggestions</h6>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default CreatePlaylistModal;
--- src/components/ui/Panel.js
+++ src/components/ui/Panel.js
@@ -1,58 +1,60 @@
 import {
   faBookBookmark,
   faHome,
-  faPlus,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-router-dom";
-import Button from "./Button";
 import CreatePlaylist from "../playlist/CreatePlaylist";
 import BrowseSongs from "../musics/BrowseSongs";
+import { useState } from "react";
+import CreatePlaylistModal from "../playlist/CreatePlaylistModal";
 
-const Panel = () => (
-  <>
-    <ul className="rounded-md shadow bg-white dark:bg-dark mb-4 p-6">
-      <li className="mb-6">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "flex items-center"
-              : "flex items-center text-gray-500 hover:text-dark dark:hover:text-white transition"
-          }
-        >
-          <FontAwesomeIcon icon={faHome} size="lg" />
-          <span className="font-semibold dark:font-extrabold ms-4">Home</span>
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/search"
-          className={({ isActive }) =>
-            isActive
-              ? "flex items-center"
-              : "flex items-center text-gray-500 hover:text-dark dark:hover:text-white transition"
-          }
-        >
-          <FontAwesomeIcon icon={faSearch} size="lg" />
-          <span className="font-semibold dark:font-extrabold ms-4">Search</span>
-        </NavLink>
-      </li>
-    </ul>
-    <ul className="rounded-md shadow bg-white dark:bg-dark mb-4 p-6">
-      <div className="flex items-center">
-        <h6>
-          <FontAwesomeIcon icon={faBookBookmark} size="lg" />
-          <span className="font-bold text-lg text-gray-500  ms-4">
-            Your Library
-          </span>
-        </h6>
-      </div>
-    </ul>
-    <CreatePlaylist />
-    <BrowseSongs />
-  </>
-);
+const Panel = () => {
+  const [modal, setModal] = useState(false);
+
+  const handleModal = () => setModal(!modal);
+
+  return (
+    <>
+      <ul className="rounded-md shadow bg-white dark:bg-dark mb-4 p-6">
+        <li className="mb-6">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center"
+                : "flex items-center text-gray-500 hover:text-dark dark:hover:text-white transition"
+            }
+          >
+            <FontAwesomeIcon icon={faHome} size="lg" />
+            <span className="font-semibold dark:font-extrabold ms-4">Home</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="flex items-center" onClick={handleModal}>
+            <FontAwesomeIcon icon={faSearch} size="lg" />
+            <span className="font-semibold dark:font-extrabold ms-4">
+              Search
+            </span>
+          </NavLink>
+        </li>
+      </ul>
+      <ul className="rounded-md shadow bg-white dark:bg-dark mb-4 p-6">
+        <div className="flex items-center">
+          <h6>
+            <FontAwesomeIcon icon={faBookBookmark} size="lg" />
+            <span className="font-bold text-lg text-gray-500  ms-4">
+              Your Library
+            </span>
+          </h6>
+        </div>
+      </ul>
+      <CreatePlaylist />
+      <BrowseSongs />
+      <CreatePlaylistModal modal={modal} handleModal={handleModal} />
+    </>
+  );
+};
 
 export default Panel;
