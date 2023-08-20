af283298b40cfa7774541aafc27dfedce1025ed0
hsyntes <se.hsyntes@gmail.com>
2023-08-20 17:14:03 +0300

    update brand ui

--- src/components/ui/Brand.js
+++ src/components/ui/Brand.js
@@ -3,8 +3,8 @@ import logo from "../../logo.svg";
 
 const Brand = () => (
   <Link to="/" className="flex items-center">
-    <img src={logo} width={42} alt="App Logo" loading="lazy" />
-    <h1 className="font-bold text-3xl ms-2">SpotifyMERN</h1>
+    <img src={logo} width={36} alt="App Logo" loading="lazy" />
+    <h1 className="font-bold text-2xl md:text-3xl ms-2">SpotifyMERN</h1>
   </Link>
 );
 
