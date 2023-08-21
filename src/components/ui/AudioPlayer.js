7605b435a784470500fdc9f6c123d7ba5d6d7c74
hsyntes <se.hsyntes@gmail.com>
2023-08-22 00:26:59 +0300

    implement play & stop functionality

--- src/components/ui/AudioPlayer.js
+++ src/components/ui/AudioPlayer.js
@@ -2,6 +2,9 @@ import { useQuery } from "react-query";
 import { useSelector } from "react-redux";
 import getMusic from "../../utils/getMusic";
 import Playing from "../musics/Playing";
+import { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const AudioPlayer = () => {
   const musicState = useSelector((state) => state.music);
@@ -11,15 +14,38 @@ const AudioPlayer = () => {
     queryFn: () => getMusic(music.id),
   });
 
+  const [isPlaying, setIsPlaying] = useState(currentMusic ? true : false);
+  const audioRef = useRef(new Audio(currentMusic));
+
+  const handlePlay = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+        setIsPlaying(false);
+      } else {
+        audioRef.current.play();
+        setIsPlaying(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (currentMusic) setIsPlaying(true);
+  }, [currentMusic]);
+
   return (
     <div className="w-full flex items-center justify-center fixed bottom-0 rounded p-4">
       <audio
+        ref={audioRef}
         src={music.source}
         // controls
-        autoPlay
+        autoPlay={isPlaying}
       />
       {currentMusic && (
-        <div className="w-96 lg:w-2/4 xl:w-1/4 rounded shadow-lg dark:border-2 dark:border-gray-700 flex items-center bg-white dark:bg-black mx-auto p-4">
+        <div
+          className="w-96 lg:w-2/4 xl:w-1/4 rounded shadow-lg dark:border-2 dark:border-gray-700 flex items-center bg-white dark:bg-black cursor-pointer mx-auto p-4"
+          onClick={handlePlay}
+        >
           <div
             style={{
               width: "48px",
@@ -37,7 +63,22 @@ const AudioPlayer = () => {
             <h1 className="font-semibold">{currentMusic.name}</h1>
             <p className="opacity-75">{currentMusic.singer}</p>
           </div>
-          <Playing className="ms-auto" />
+          {/* <Playing className="ms-auto" /> */}
+          {isPlaying ? (
+            <Playing className="ms-auto" />
+          ) : (
+            <div className="ms-auto">
+              <button
+                className="flex items-center justify-center bg-primary rounded-full"
+                style={{
+                  width: "36px",
+                  height: "36px",
+                }}
+              >
+                <FontAwesomeIcon icon={faPlay} className="text-dark" />
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
